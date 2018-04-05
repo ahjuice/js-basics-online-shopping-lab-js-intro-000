13218a054a43c8c60f8f88ca1abe054e34850842
Christopher Agius <chrisagius@fastmail.com>
2018-04-05 17:34:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -48,8 +48,15 @@ function total() {
   return runningTotal;
 }
 
+
 function removeFromCart(item) {
-  // write your code here
+  for (let i = 0; i < cart.length; i++) {
+    if (cart[i].itemName === item) {
+      cart.splice(i, 1);
+      return cart;
+    }
+  }
+  return 'That item is not in your cart.'
 }
 
 function placeOrder(cardNumber) {
