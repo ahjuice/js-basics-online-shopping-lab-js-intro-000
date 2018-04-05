7cb4ea5ebc010b81b3eaac13f22c95ca1147cd63
Christopher Agius <chrisagius@fastmail.com>
2018-04-05 16:54:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 var cart = [];
 
+/* Function used from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+   section entitled "Getting a random integer between two values, inclusive" */
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive
+}
+
 function getCart() {
  return cart;
 }
@@ -10,7 +18,10 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+ let itemObj = new Object();
+ itemObj.itemName = item;
+ itemObj.itemPrice = getRandomIntInclusive(1, 100);
+ return `${item} has been added to your cart.`
 }
 
 function viewCart() {
